c33aa3b15cff96744cce76563aa3a170897249f8
tonton <xtian23anthony@outlook.com>
2024-08-15 11:38:40 +0800

    ui update to subject load modal

--- src/components/dialogs/SubjectLoadDialog.jsx
+++ src/components/dialogs/SubjectLoadDialog.jsx
@@ -1,54 +1,43 @@
-import { Close } from "@mui/icons-material"
-import { Box, Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material"
+import { Close } from "@mui/icons-material";
+import { Box, Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 
-const SubjectLoadDialog = ({
-    open,
-    close,
-    data,
-}) => {
-    return (
-        <Dialog 
-            open={open} 
-            onClose={close} 
-            aria-labelledby={"dialog-confirmation"}
-            maxWidth={"xl"}
+const SubjectLoadDialog = ({ open, close, data }) => {
+  return (
+    <Dialog open={open} onClose={close} aria-labelledby={"dialog-confirmation"} maxWidth={"xl"} fullWidth>
+      <DialogTitle id={"dialog-confirmation-title"}>SUBJECT LOAD</DialogTitle>
+      <IconButton
+        aria-label="close"
+        onClick={close}
+        sx={{
+          position: "absolute",
+          right: 8,
+          top: 8,
+        }}
+      >
+        <Close />
+      </IconButton>
+      <DialogContent dividers>
+        <Box
+          sx={{
+            height: 500,
+            width: "100%",
+          }}
         >
-          <DialogTitle id={"dialog-confirmation-title"}>
-            SUBJECT LOAD
-          </DialogTitle>
-          <IconButton 
-            aria-label="close" 
-            onClick={close}
-            sx={{
-              position: 'absolute',
-              right: 8,
-              top: 8,
+          <DataGrid
+            rows={data.rows}
+            columns={data.columns}
+            initialState={{
+              pagination: {
+                paginationModel: { page: 0, pageSize: 5 },
+              },
             }}
-          >
-            <Close />
-          </IconButton>
-          <DialogContent dividers>
-          <Box 
-            sx={{ 
-              height: 500,
-              width: 1500
-             }}
-            >
-                <DataGrid
-                  rows={data.rows}
-                  columns={data.columns}
-                  initialState={{ 
-                      pagination: {
-                          paginationModel: { page: 0, pageSize: 5 },
-                      }
-                  }}
-                  pageSizeOptions={[5, 10]}
-                />
-            </Box>
-          </DialogContent>
-        </Dialog>
-    )
-}
+            pageSizeOptions={[5, 10]}
+          />
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
+};
 
 export default SubjectLoadDialog;
